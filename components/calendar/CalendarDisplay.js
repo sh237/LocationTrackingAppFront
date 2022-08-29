7d00e399818b01fe5ed8fe8694ce287f16c1faaa
Shunya Nagashima <syun864297531@gmail.com>
2022-08-29 13:50:14 +0900

    カレンダーの実装

--- components/calendar/CalendarDisplay.js
+++ components/calendar/CalendarDisplay.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {StyleSheet, View, Text, Button} from 'react-native';
+import { Calendar,LocaleConfig } from 'react-native-calendars';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import moment from "moment";
 
+const INITIAL_DATE = moment().format("YYYY-MM-DD");
 const CalendarDisplay = ({navigation}) => {
+    LocaleConfig.locales.jp = {
+        monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+        dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+      };
+    LocaleConfig.defaultLocale = 'jp';
+    const [selected, setSelected] = useState(INITIAL_DATE);
+    const handleDayPress = (day) => {
+        setSelected(day.dateString);
+        console.log(new Date(selected).toLocaleString());
+        navigation.navigate("Map",{date:new Date(selected).toLocaleString()});
+        
+        }
+    
     return(
     <View style={styles.container}>
-    <Text>DetailScreen</Text>
-    <Button
-      title="Map画面に遷移する"
-      onPress={() => {
-        navigation.navigate('Map');
-      }}
-    />
+        <Calendar
+        monthFormat={"yyyy年 MM月"}
+        current={INITIAL_DATE}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedColor: 'red',
+            selectedTextColor: 'white'
+          }
+        }}
+        onDayPress={handleDayPress}
+      />
+        <SafeAreaView/>
+        <Text>DetailScreen</Text>
+        <Button
+        title="Map画面に遷移する"
+        onPress={() => {
+            navigation.navigate('Map');
+        }}
+        />
   </View>
 );
 };
+
 
 const styles = StyleSheet.create({
 container: {
